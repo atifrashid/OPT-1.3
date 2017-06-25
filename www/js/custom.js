a1e43288a76e7bcf1b7bbde232613105094d69fd
atifrashid <atifrashid@hotmail.com>
2017-06-25 16:46:18 +0500

    3

--- www/js/custom.js
+++ www/js/custom.js
@@ -2,114 +2,126 @@
 //form serialize
 
 function saveFormDate() {
-    var str = $("#formclaim").serialize();
-    $("#results").text(str);
-	 localStorage.setItem('formdata', str );
+	var str = $("#formclaim").serialize();
+	$("#results").text(str);
+	localStorage.setItem('formdata', str);
 }
 
 
 $(document).ready(function () {
-    // jQuery version number text
-    $("input[type='number']").each(function (i, el) {
-        el.type = "text";
-        el.onfocus = function () {
-            this.type = "number";
-        };
-        el.onblur = function () {
-            this.type = "text";
-        };
-    });
+	// jQuery version number text
+	$("input[type='number']").each(function (i, el) {
+		el.type = "text";
+		el.onfocus = function () {
+			this.type = "number";
+		};
+		el.onblur = function () {
+			this.type = "text";
+		};
+	});
 
 
-	    //call function on blur 
-    $(":input").blur(function () {
-        saveFormDate();
-    });
+	//call function on blur 
+	$(":input").blur(function () {
+		saveFormDate();
+	});
 
 
 	//var picker = $( "input[type='text']", this );
-    $('.mdate').mobipick();
+	$('.mdate').mobipick();
 
-    // jQuery version number text
+	// jQuery version number text
 
-    //cookies in input fields
+	//cookies in input fields
 
-    if ( localStorage.getItem('formdata') ) {
-        var str = decodeURIComponent(( localStorage.getItem('formdata')  + '').replace(/\+/g, '%20'));
-        //var str = urldecode(jsData);
-        //alert(str);
-        var pairs = str.split('&');
-        var obj = {}, p, idx, val;
-        for (var i = 0, n = pairs.length; i < n; i++) {
-            p = pairs[i].split('=');
-            //alert(pairs[i]);
-            idx = p[0];
-            var ele = $('[name="' + idx + '"]');
-            var type = ele.attr('type');
-            if (type == 'checkbox') {
-                $(':checkbox[value="' + p[1] + '"]').attr("checked", "checked");
+	if (localStorage.getItem('formdata')) {
+		var str = decodeURIComponent((localStorage.getItem('formdata') + '').replace(/\+/g, '%20'));
+		//var str = urldecode(jsData);
+		//alert(str);
+		var pairs = str.split('&');
+		var obj = {},
+			p, idx, val;
+		for (var i = 0, n = pairs.length; i < n; i++) {
+			p = pairs[i].split('=');
+			//alert(pairs[i]);
+			idx = p[0];
+			var ele = $('[name="' + idx + '"]');
+			var type = ele.attr('type');
+			if (type == 'checkbox') {
+				$(':checkbox[value="' + p[1] + '"]').attr("checked", "checked");
 
-            } else if (type == 'text') {
-                ele.val(p[1]);
-            } else if (type == 'date') {
-                ele.val(p[1]);
-            } else if (type == 'email') {
-                ele.val(p[1]);
-            } else if (type == 'radio') {
-                $(':radio[value="' + p[1] + '"]').attr("checked", "checked");
-                $(':radio[value="' + p[1] + '"]').trigger('change');
-            } else if (ele.is('select')) {
-                ele.val(p[1]);
-                ele.trigger('change');
-            } else if (ele.is('textarea')) {
-                ele.val(p[1]);
-            }
-        }
-    }
-    //chkbox show hide
-    $('#chk_1').change(function () {
-        if ($(this).is(':checked')) {
-            $('#tbl_addwarr').show(200);
-        } else {
-            $('#tbl_addwarr').hide(200);
-        }
+			} else if (type == 'text') {
+				ele.val(p[1]);
+			} else if (type == 'date') {
+				ele.val(p[1]);
+			} else if (type == 'email') {
+				ele.val(p[1]);
+			} else if (type == 'radio') {
+				$(':radio[value="' + p[1] + '"]').attr("checked", "checked");
+				$(':radio[value="' + p[1] + '"]').trigger('change');
+			} else if (ele.is('select')) {
+				ele.val(p[1]);
+				ele.trigger('change');
+			} else if (ele.is('textarea')) {
+				ele.val(p[1]);
+			}
+		}
+	}
+	//chkbox show hide
+	$('#chk_1').change(function () {
+		if ($(this).is(':checked')) {
+			$('#tbl_addwarr').show(200);
+		} else {
+			$('#tbl_addwarr').hide(200);
+		}
 
-    });
+	});
 
-    $('#chk_2').change(function () {
-        if ($(this).is(':checked')) {
-            $('#tbl_addwarr2').show(200);
-        } else {
-            $('#tbl_addwarr2').hide(200);
-        }
-    });
+	$('#chk_2').change(function () {
+		if ($(this).is(':checked')) {
+			$('#tbl_addwarr2').show(200);
+		} else {
+			$('#tbl_addwarr2').hide(200);
+		}
+	});
 
 	$('#chk_3').change(function () {
-        if ($(this).is(':checked')) {
-            $('#tbl_pg').show(200);
-        } else {
-            $('#tbl_pg').hide(200);
-        }
-    });
+		if ($(this).is(':checked')) {
+			$('#tbl_pg').show(200);
+		} else {
+			$('#tbl_pg').hide(200);
+		}
+	});
 
 
-	
+	$('#chk_4').change(function () {
+		if ($(this).is(':checked')) {
+			$('#tbl_ic').show(200);
+		} else {
+			$('#tbl_ic').hide(200);
+		}
+	});
 
 
 
 });
 
 /* Date Picker */
+var startDate = moment().subtract(30, 'days').format('YYYY-MM-DD');
+var endDate = moment().format('YYYY-MM-DD');
 
-	
-	$(".date-picker").datepicker({
-        format: 'yyyy-mm-dd',
-		  autoclose: true 
-   });
-	
-	$(".date-picker").on("change", function () {
-		var id = $(this).attr("id");
-		var val = $("label[for='" + id + "']").text();
-		$("#msg").text(val + " changed");
-	});
-	
+$(".date-picker").val(endDate);
+
+$(".date-picker").datepicker({
+	format: 'yyyy-mm-dd',
+	startDate: startDate,
+	endDate: endDate
+
+});
+
+
+$(".date-picker").on("change", function () {
+	var id = $(this).attr("id");
+	var val = $("label[for='" + id + "']").text();
+	$("#msg").text(val + " changed");
+});
--- www/js/login.js
+++ www/js/login.js
@@ -1,12 +1,12 @@
 //logo page redirection
-$(document).on('pagebeforeshow', '#opt_app', function() {
+$(document).on('pagebeforeshow', '#opt_app', function () {
 
 	if (localStorage.getItem('DealerId')) {
-		setTimeout(function() {
+		setTimeout(function () {
 			window.location.href = "#form";
 		}, 2000);
 	} else {
-		setTimeout(function() {
+		setTimeout(function () {
 			window.location.href = "#login";
 		}, 2000);
 	}
@@ -14,13 +14,14 @@ $(document).on('pagebeforeshow', '#opt_app', function() {
 
 
 
-$(document).on('pageshow', '#form', function() {
+$(document).on('pageshow', '#form', function () {
 
 	$('#chk_1').trigger("change");
 	$('#chk_2').trigger("change");
 	$('#chk_3').trigger("change");
-	
-	if( localStorage.getItem('DealerClass' ) == 'Dealership' )
+	$('#chk_4').trigger("change");
+
+	if (localStorage.getItem('DealerClass') == 'Dealership')
 		$("#chk_3").parent().show();
 	else
 		$("#chk_3").parent().hide();
@@ -31,135 +32,159 @@ $(document).on('pageshow', '#form', function() {
 
 
 /**/
-$(document).on('pagebeforeshow', '#pagePrivacy', function() {
+$(document).on('pagebeforeshow', '#pagePrivacy', function () {
 
 	$.ajax({
 		type: "GET",
-		url: "http://opti-coat.net/opt_app/login.php?handler=getCMS&page_id=59",
+		url: "http://opticoat.com/opt_app/login.php?handler=getCMS&page_id=59",
 		dataType: 'json',
-		beforeSend: function() {
+		beforeSend: function () {
 			$.mobile.loading('show')
 		},
-		complete: function() {
+		complete: function () {
 			$.mobile.loading('hide')
 		},
-		success: function(data) {
+		success: function (data) {
 			if (data.Success == 'true') {
 				$('#pagePrivacy div#sp_privacy').html(data.HTML);
 			} else {
 				$('#pagePrivacy div#sp_privacy').html(data.Msg);
 			}
 		},
-		error: function() {
+		error: function () {
 			alert('Could not connect to the server!');
 		}
 	});
 });
 /**/
 
-function QRonSuccess(result){
-	
-	if( result.cancelled == true ){
-		return false; 	
-	}				 
+function QRonSuccess(result) {
+
+	if (result.cancelled == true) {
+		return false;
+	}
 	var code = result.text;
 	$('#vehicle_vin').val(code);
 	//alert(code);
 }
-function QRFailure(message){
+
+function QRFailure(message) {
 	$('#messagebox p').html('Scanner Fails!');
 }
 
 
-$(document).ready(function() {
-	
-	$('#btnBarcode').on('click',function(e){
-		try{
+$(document).ready(function () {
+
+	$('#btnBarcode').on('click', function (e) {
+		try {
 			cordova.plugins.barcodeScanner.scan(QRonSuccess, QRFailure);
-		}catch(err) {
+		} catch (err) {
 			$('#messagebox p').html('Scanner Fails!');
 		}
 		e.preventDefault();
 	});
-	
 
-	$("#submit").click(function() {
-		//alert('sss');
-		//form validatio
-		$("#formclaim").validate({
 
-			submitHandler: function(form) {
-				//alert("hello");  // stuff to do when form is valid
-				// alert('valid form submitted'); // for demo
+	$("#formclaim").validate({
+		
+		errorPlacement: function(error, element) {},
+		submitHandler: function (form) {
+			
+			
+			var LotPat = /(L)[0-9]{6}[A-z]{1}/i;
+			
+			if( $('#chk_2').prop('checked') == true ){
+				if ( LotPat.test( $( '#opticoat_pro_lot_no' ).val() ) == false ) {
+					//alert( 'OPTI-COAT PRO LOT NO. is not valid!' );
+					
+					$('#messagebox p').html('OPTI-COAT PRO LOT NO. is not valid!');
+					$.mobile.changePage('#messagebox', 'pop', true, true);
+						
+					
+					$( '#opticoat_pro_lot_no' ).focus();
+					return false;
+				}
+			}	
+			
+			if( $('#chk_1').prop('checked') == true ){
+				if( $( '#optiguard_leather_lot_no' ).val() != '' ){
+					if ( LotPat.test( $( '#optiguard_leather_lot_no' ).val() ) == false ) {
+						//alert( 'Leather Lot No. is not valid!' );
+						$('#messagebox p').html('Leather Lot No. is not valid!');
+						$.mobile.changePage('#messagebox', 'pop', true, true);
+						
+						$( '#optiguard_leather_lot_no' ).focus();
+						return false;
+					}
+				}
+			}
+			
+			
+			var formData = $("#formclaim").serialize();
 
-				//alert('functin called');
-				var formData = $("#formclaim").serialize();
+			$.ajax({
+				type: "POST",
+				url: "http://opticoat.com/opt_app/login.php?handler=add&dealer_id=" + localStorage.getItem('DealerId'),
+				beforeSend: function () {
+					$.mobile.loading('show')
+				},
+				complete: function () {
+					$.mobile.loading('hide')
+				},
+				data: formData,
+				dataType: 'json',
+				success: function (data) {
 
-				$.ajax({
-					type: "POST",
-					url: "http://opti-coat.net/opt_app/login.php?handler=add&dealer_id=" + localStorage.getItem('DealerId'),
-					beforeSend: function() {
-						$.mobile.loading('show')
-					},
-					complete: function() {
-						$.mobile.loading('hide')
-					},
-					data: formData,
-					dataType: 'json',
-					success: function(data) {
-
-						if (data.Msg) {
-							$('#messagebox p').html(data.Msg);
-							$.mobile.changePage('#messagebox', 'pop', true, true);
-						}
-
-						if (data.Success == 'true') {
-							localStorage.clear();
-							form.reset();
-						}
-
-						if (data.JS) {
-							//eval(data.JS);
-						}
-					},
-
-					error: function(data, errorThrown) {
-						alert('request failed :' + errorThrown);
+					if (data.Msg) {
+						$('#messagebox p').html(data.Msg);
+						$.mobile.changePage('#messagebox', 'pop', true, true);
 					}
 
+					if (data.Success == 'true') {
+						localStorage.removeItem('formdata');
+						form.reset();
+					}
 
-				});
+					if (data.JS) {
+						//eval(data.JS);
+					}
+				},
 
-				return false;
+				error: function (data, errorThrown) {
+					alert('request failed :' + errorThrown);
+				}
+			});
+
+			return false;
 
 
-				//$.mobile.changePage("#form");
-				return false;
-			},
-			invalidHandler: function() {
-				// stuff to do when form is invalid
-				// alert("invalid form");
-			}
-		});
+			//$.mobile.changePage("#form");
+			return false;
+		},
+		invalidHandler: function () {
+			// stuff to do when form is invalid
+			// alert("invalid form");
+		}
 	});
-	$(".logout").click(function() {
+
+	$(".logout").click(function () {
 		localStorage.clear();
 		$.mobile.changePage("#login");
 		$("#form_login")[0].reset();
 	})
 	$("#errorMsg").hide();
-	$("#btnLogin").click(function() {
+	$("#btnLogin").click(function () {
 
 		var usu = $("#txtuser").val();
 		var pass = $("#txtpassword").val();
 
 		$.ajax({
 			type: "POST",
-			url: "http://opti-coat.net/opt_app/login.php?handler=Login",
-			beforeSend: function() {
+			url: "http://opticoat.com/opt_app/login.php?handler=Login",
+			beforeSend: function () {
 				$.mobile.loading('show')
 			},
-			complete: function() {
+			complete: function () {
 				$.mobile.loading('hide')
 			},
 			data: {
@@ -167,7 +192,7 @@ $(document).ready(function() {
 				'pass': pass
 			},
 			dataType: 'json',
-			success: function(data) {
+			success: function (data) {
 
 				if (data.Success == 'true') {
 
@@ -186,7 +211,7 @@ $(document).ready(function() {
 					$.mobile.changePage('#messagebox', 'pop', true, true);
 				}
 			},
-			error: function() {
+			error: function () {
 				alert('Could not connect to the server!');
 			}
 
